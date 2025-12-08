ea6ebb6166d5e469b4878bf03a9bb6759cbb2cad
William Andersson <williama00@hotmail.se>
2025-12-08 12:44:51 +0100

    2025-05 part 2

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -1,6 +1,26 @@
 score = 0
 min_max = list()
 
+def optimize_minmax(min_max):
+    min_max.sort()
+    optimized = list()
+    current_min, current_max = min_max[0]
+    for m_m in min_max[1:]:
+        if m_m[0] <= current_max:
+            if m_m[1] > current_max:
+                current_max = m_m[1]
+        else:
+            optimized.append((current_min, current_max))
+            current_min, current_max = m_m
+    optimized.append((current_min, current_max))
+    return optimized
+
+def fresh_amount(min_max):
+    total = 0
+    for m_m in min_max:
+        total += m_m[1] - m_m[0] + 1
+    return total
+
 with open("input.txt") as f:
     for line in f:
         line = line.strip()
@@ -10,6 +30,11 @@ with open("input.txt") as f:
         a = int(a)
         b = int(b)
         min_max.append((a, b))
+
+    # Part 2
+    min_max = optimize_minmax(min_max)
+    print(fresh_amount(min_max))
+    # Part 2 end
 
     for line in f:
         val = int(line)
